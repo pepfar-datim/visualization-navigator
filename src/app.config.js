d325ee049d7424e73f7244a14c70abcef4479273
Jakub Flaska <jflaska@baosystems.com>
2022-06-01 13:43:50 -0400

    before major rename

--- src/app.config.js
+++ src/app.config.js
@@ -6,3 +6,4 @@ export const appConfig = {
   sqlQueryNameIndex: 1,
   sqlViewCountParams: `uid:[uid],dseUpper:2200-01-01,minDate:1980-01-01,favName:_,dseLower:1980-01-01,limitCount:1,visTypes:_,retrieveNevers:1,maxDate:2200-01-01,minCount:0,user:_,maxCount:1000,`
 };
+
