eb6ba54f564d446311a2ee1653012c50ad5d9bb5
Jakub Flaska <jflaska@baosystems.com>
2022-06-13 15:55:09 -0400

    working

--- search/src/modules/searchSettings/components/searchSettingsDialog.component.tsx
+++ search/src/modules/searchSettings/components/searchSettingsDialog.component.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import {SearchSettings, UpdateSearchSettings} from "../types/searchSettings.type";
+import {Button} from "@mui/material";
 
 export function SearchSettingsDialog({open, close, searchSettings, updateSettings}:{open: boolean, close:()=>void, searchSettings:SearchSettings,updateSettings:UpdateSearchSettings}) {
     return <Dialog onClose={close} open={open}>
         <DialogTitle>Search settings</DialogTitle>
+        <Button onClick={()=>updateSettings({limit:200,limitViewsMinDate: '1978-01-01',limitViewsMaxDate: '2500-01-01'})}>test</Button>
     </Dialog>
 }
