e9d1748903c63222ffe2e9ea5561402bca57b68a
Jakub Flaska <jflaska@baosystems.com>
2022-06-13 14:21:29 -0400

    working

--- search/src/modules/searchFilters/components/filterPropertySelect.component.tsx
+++ search/src/modules/searchFilters/components/filterPropertySelect.component.tsx
@@ -6,13 +6,12 @@ import {FormControl} from "./formControl.component";
 import {camelCaseToCapitalized, capitalize} from "../../searchPage/services/textFormat.service";
 
 export function FilterPropertySelect({filterProperty, changeFilterType,availableFilters, i}:{filterProperty:FilterProperty|null, changeFilterType: ChangeFilterType, availableFilters:FilterProperty[], i:number}) {
-    console.log(filterProperty, availableFilters)
     return <FormControl className='filterPropertySelect'>
         <Select
             value={filterProperty}
             onChange={(e)=>changeFilterType(e.target.value as FilterProperty)}
             data-testid={`filterProperty_${i}`}
-            renderValue={(v:any)=><>{capitalize(v)}</>}
+            renderValue={(v:any)=><>{camelCaseToCapitalized(v)}</>}
         >
             {availableFilters.map(f=><MenuItem value={f} key={f}>{camelCaseToCapitalized(f)}</MenuItem>)}
         </Select>
--- search/src/test/1.filterBehavior.test.tsx
+++ search/src/test/1.filterBehavior.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {render} from "@testing-library/react";
+import {fireEvent, render, screen, within} from "@testing-library/react";
 import {SearchPage} from "../modules/searchPage/components/searchPage.component";
 import {checkSelectValue, click, notToExistList, select} from "@pepfar-react-lib/testwrap";
 import {addFilter} from "./lib/shared.testLib";
@@ -9,15 +9,28 @@ export function setFilter(i:number,property:string,expectedOperator:string){
     checkSelectValue(`filterOperator_${i}`,expectedOperator)
 }
 
+function checkPropertyAvailable(selectId:string,property:string, available:boolean) {
+    fireEvent.mouseDown(screen.getByTestId(selectId).childNodes[0]);
+    let modal = screen.getByRole('presentation');
+    if (available) within(modal).getByText(property)
+    else expect(within(modal).queryByText(property)).not.toBeInTheDocument();
+}
+
 test(`1 > Search filter behavior`, async ()=>{
     render(<SearchPage/>);
     addFilter();
     setFilter(0,'Name','contains')
     addFilter();
+    checkPropertyAvailable(`filterProperty_1`,'Name', false)
     setFilter(1,'Views','greater than')
-    setFilter(1,'Last Viewed','before')
-    click(`deleteFilter_1`)
-    notToExistList(['deleteFilter_1','filterProperty_1'])
-    setFilter(0,'Type','is')
+    addFilter();
+    checkPropertyAvailable(`filterProperty_2`,'Views', true);
+    setFilter(2,'Views','greater than')
+    addFilter();
+    checkPropertyAvailable(`filterProperty_3`,'Views', false);
+
+    click(`deleteFilter_3`)
+    notToExistList(['deleteFilter_3','filterProperty_3'])
+
 })
 
